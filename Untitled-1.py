99e4e91ed12a6f9f751c2dfefb293c465723c547
main <elie-s@live.fr>
2019-11-15 18:34:35 +0100

    kdfjhgv

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,12 @@
 import numpy as np
+#si une erreur apparait dans l'importation de numpy 
+# #faire: pip install numpy
 
-#b = np.array([[0,0,0,0,0,0,0],[0,0,0,0,0,0]])
+
 a = np.zeros((7,6))
 #quel_colonne = int(input("A quel colonne voulez vous mettre le pion ?\n"))
 a[6,4] = 1
-print(a)
+
+print(a,b)
+
 
