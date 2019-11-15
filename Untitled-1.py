618470fe00e8af8a11f0968d752aaf7386fec8cc
main <elie-s@live.fr>
2019-11-15 21:26:58 +0100

    first commit

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,38 @@
 import numpy as np
-#si une erreur apparait dans l'importation de numpy 
-# #faire: pip install numpy
+from os import system
+
+#definition d'une fonction qui enleve tout le texte du terminal
+def clear():
+    '''clear console'''
+    return system('cls')
 
 
-a = np.zeros((7,6))
-#quel_colonne = int(input("A quel colonne voulez vous mettre le pion ?\n"))
-a[6,4] = 1
 
-print(a,b)
+w, h = 7, 6;
+Matrix = [[0 for x in range(w)] for y in range(h)] 
+Matrix[5][0] = 'O'
+pion = 'X'
 
 
+while True:
+
+    
+    colonne_choisit = int(input("quel colonne ? \nentrer un nombre de 1 à 6:   "))-1
+
+    for i in range(0,6):
+
+        #print(" le i est " , i, 'et le matrix[i][col est ]',Matrix[i][colonne_choisit])
+        if Matrix[i][colonne_choisit] == 0 and i == 5:
+            Matrix[5][colonne_choisit] = pion
+
+            break
+
+        if Matrix[i][colonne_choisit] != 0:
+            Matrix[i-1][colonne_choisit] = pion
+
+            break
+        
+    
+    for i in range(6):
+        print('   '.join(map(str, Matrix[i])),'\n') #convertie chaque caractere de matrix en str et le regroupe en une ligne séparé par 2espaces 
+    
