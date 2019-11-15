b904c8cb9418c082d857e18517c8b2bb27d9b42e
pc_bts <elie-s@live.fr>
2019-11-15 11:56:58 +0100

    nothing

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-b = np.array([[0,0,0,0,0,0,0],[0,0,0,0,0,0]])
+#b = np.array([[0,0,0,0,0,0,0],[0,0,0,0,0,0]])
 a = np.zeros((7,6))
 #quel_colonne = int(input("A quel colonne voulez vous mettre le pion ?\n"))
 a[6,4] = 1
