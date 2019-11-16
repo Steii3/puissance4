cb444ebc896580e8b120d47a8e1ca35e6afc2031
laptop <elie-s@live.fr>
2019-11-16 15:17:33 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,9 @@
-import numpy as np
 from os import system
 
 #definition d'une fonction qui enleve tout le texte du terminal
 def clear():
     '''clear console'''
     return system('cls')
-
-
 
 w, h = 7, 6;
 Matrix = [[0 for x in range(w)] for y in range(h)] 
@@ -16,23 +13,19 @@ pion = 'X'
 
 while True:
 
-    
-    colonne_choisit = int(input("quel colonne ? \nentrer un nombre de 1 à 6:   "))-1
-
+    colonne_choisit = int(input("quel colonne ? \nentrer un nombre de 1 à 7:   "))-1
+    clear()
     for i in range(0,6):
 
         #print(" le i est " , i, 'et le matrix[i][col est ]',Matrix[i][colonne_choisit])
         if Matrix[i][colonne_choisit] == 0 and i == 5:
             Matrix[5][colonne_choisit] = pion
-
             break
 
         if Matrix[i][colonne_choisit] != 0:
             Matrix[i-1][colonne_choisit] = pion
-
             break
         
-    
     for i in range(6):
-        print('   '.join(map(str, Matrix[i])),'\n') #convertie chaque caractere de matrix en str et le regroupe en une ligne séparé par 2espaces 
+        print(' '.join(map(str, Matrix[i])),) #convertie chaque caractere de matrix en str et le regroupe en une ligne séparé par 2espaces 
     
